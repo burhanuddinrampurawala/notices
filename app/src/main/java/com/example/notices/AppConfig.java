935adbfa39f0659c35ab25f89522fcecf8d2a262
burhanuddin rampurawala <burhanuddinzrampurawala@gmail.com>
2016-09-13 13:34:14 +0530

    Update AppConfig.java

--- app/src/main/java/com/example/notices/AppConfig.java
+++ app/src/main/java/com/example/notices/AppConfig.java
@@ -3,10 +3,10 @@ package com.example.notices;
 
 public class AppConfig {
     // Server student login url
-    public static String URL_STUDENT_LOGIN = "http://your ip address/notices/login.php";
+    public static String URL_STUDENT_LOGIN = "http://your own ip address/notices/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://your ip address/notices/register.php";
+    public static String URL_REGISTER = "http://your own ip address/notices/register.php";
 
     // Server admin login url
     public static String URL_ADMIN_LOGIN = "";
